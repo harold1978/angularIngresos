c1d558b2aee5450b3e625531f1fd547a9e39acb8
HAROLD MUÑOZ <36018526+harold1978@users.noreply.github.com>
2025-02-06 10:47:38 -0600

    funcional parte ingresos

--- src/app/componentes/ingresos/frm-ingresos/frm-ingresos.component.ts
+++ src/app/componentes/ingresos/frm-ingresos/frm-ingresos.component.ts
@@ -10,6 +10,7 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './frm-ingresos.component.css'
 })
 export class FrmIngresosComponent {
+  
   ingresos:Iingresos[]=[];
 
   nuevoingreso:Iingresos={id:"",nombre:"",fecha:"",monto:0};
@@ -22,13 +23,31 @@ export class FrmIngresosComponent {
     const item = this.ingresos.find(item=>item.id==this.nuevoingreso.id);
     if(item){
       const indice = this.ingresos.indexOf(item);
-      this.ingresos.splice(indice,1,this.nuevoingreso);
+      this.ingresos.splice(indice,1,{...this.nuevoingreso});
       alert("ELEMENTO ACTUALIZADO");
+      this.limpiar();
       return;
     }
-    this.ingresos.push(this.nuevoingreso);
+    const {id,nombre,fecha,monto}=this.nuevoingreso;
+    
+    this.ingresos.push({...this.nuevoingreso});
     alert("INGRESO AGREGADO");
-  
+    this.limpiar();
   }
-
+  limpiar(){
+    this.nuevoingreso.id="";
+    this.nuevoingreso.nombre="";
+    this.nuevoingreso.fecha="";
+    this.nuevoingreso.monto=0;
+  }
+  editar(id:string):void{
+    const aux = this.ingresos.find(item=>item.id==id);
+    if(aux){
+      this.nuevoingreso={...aux};
+    }
+  }
+  eliminar(id:string){
+    this.ingresos = this.ingresos.filter(item=>item.id!==id);
+    
+  }
 }
